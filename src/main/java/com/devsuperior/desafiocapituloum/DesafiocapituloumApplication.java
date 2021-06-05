f5c09153eab340c7684da1f07a861d659e8ca419
Carlos Augusto <augustino2003@hotmail.com>
2021-06-05 16:35:04 -0300

    Project created

---
+++ src/main/java/com/devsuperior/desafiocapituloum/DesafiocapituloumApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiocapituloum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocapituloumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocapituloumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafiocapituloum/DesafiocapituloumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiocapituloum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocapituloumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
